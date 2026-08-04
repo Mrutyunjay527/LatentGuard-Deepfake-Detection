85a496603efbf7f5688ba99c760d2dd61771a0f9
mjay1 <mjay1@LAPTOP-DRB68GH9>
2026-08-04 21:35:15 +0530

    added plots

---
+++ plots/plot_confusion_matrix.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix
+
+# Replace these with YOUR confusion matrix values
+
+
+cm = [
+    [532, 57],
+    [22, 1204]
+]
+
+labels = ["Real", "Fake"]
+
+plt.figure(figsize=(6, 5))
+
+sns.heatmap(
+    cm,
+    annot=True,
+    fmt="d",
+    cmap="Blues",
+    xticklabels=labels,
+    yticklabels=labels
+)
+
+plt.xlabel("Predicted Label")
+plt.ylabel("True Label")
+plt.title("Confusion Matrix - LatentGuard")
+
+plt.tight_layout()
+
+plt.savefig(
+    "outputs/confusion_matrix.png",
+    dpi=300,
+     bbox_inches="tight"
+)
+
+plt.show()
+
+print("Confusion matrix saved successfully!")
